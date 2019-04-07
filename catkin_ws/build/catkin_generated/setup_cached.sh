3c52536dd17bffb88391e1fde2bafe2037c944a6
me212 <me212@franklin.mit.edu>
2019-04-07 14:28:35 -0400

    Made catkin workspace and package for REMY

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/me212/delta/catkin_ws/devel:/opt/ros/melodic:/opt/ros/kinetic"
+export LD_LIBRARY_PATH="/opt/ros/melodic/lib:/usr/local/lib:/usr/local/cuda/lib64"
+export PWD="/home/me212/delta/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/me212/delta/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/me212/delta/catkin_ws/src:$ROS_PACKAGE_PATH"
